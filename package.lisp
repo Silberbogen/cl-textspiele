bac9cbf35bc7dee0dea249e70b239d2cd3e75055
Sascha Biermanns <skkd.h4k1n9@yahoo.de>
2015-01-29 23:12:25 +0100

    Relizensierung unter der GPL v3

--- package.lisp
+++ package.lisp
@@ -2,22 +2,26 @@
 ;;;; -*- coding: utf-8 -*-
 ;;;; Dateiname: packages.lisp
 ;;;; ------------------------------------------------------------------------
-;;;; Author: Sascha Biermanns, <skkd.h4k1n9@yahoo.de>
-;;;; Lizenz: ISC
-;;;; Copyright (C) 2014 Sascha Biermanns
-;;;; Permission to use, copy, modify, and/or distribute this software for any
-;;;; purpose with or without fee is hereby granted, provided that the above
-;;;; copyright notice and this permission notice appear in all copies.
-;;;; THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-;;;; WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-;;;; MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-;;;; ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-;;;; WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-;;;; ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-;;;; OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+;;;; Author: Sascha K. Biermanns, <skkd PUNKT h4k1n9 AT yahoo PUNKT de>
+;;;; Lizenz: GPL v3
+;;;; Copyright (C) 2011-2015 Sascha K. Biermanns
+;;;; This program is free software; you can redistribute it and/or modify it
+;;;; under the terms of the GNU General Public License as published by the
+;;;; Free Software Foundation; either version 3 of the License, or (at your
+;;;; option) any later version.
+;;;;
+;;;; This program is distributed in the hope that it will be useful, but
+;;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+;;;; Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License along
+;;;; with this program; if not, see <http://www.gnu.org/licenses/>. 
 ;;;; ------------------------------------------------------------------------
 
+
 (require :cl-hilfsroutinen)
+
 
 (defpackage #:cl-textspiele
   (:nicknames :textspiele)
